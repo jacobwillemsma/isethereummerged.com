ff372fb067f6fcf1bee3c8d797715d1410d61157
Jacob Willemsma <jacob.willemsma@gmail.com>
2022-09-14 17:20:28 -0400

    Clean up

--- src/App.js
+++ src/App.js
@@ -1,42 +1,62 @@
-import "./App.css";
 import { TwitterTweetEmbed } from "react-twitter-embed";
 
 import mergeImage from "./merge.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={mergeImage} alt="merge" />
-        <p class="text-9xl">No</p>
-        <p class="text-2xl mb-24">(But it will be soon)</p>
+    <div>
+      <header className="header">
+        <div className="hero">
+          <p
+            class="font-black"
+            style={{ fontSize: "12rem", marginBottom: "-50px" }}
+          >
+            No.
+          </p>
+          <p class="text-xl mb-24 font-semibold">(But it will be soon)</p>
+        </div>
 
         <p class="text-4xl mb-8">
           Check out any of the sources below for an updated timeline:
         </p>
-        <ul>
-          <li className="mb-2">
-            <a href="https://ultrasound.money/">https://ultrasound.money</a>
+        <ul class="mb-12">
+          <li className="mb-2 text-xl">
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://ultrasound.money/"
+            >
+              https://ultrasound.money
+            </a>
           </li>
-          <li className="mb-2">
-            <a href="https://bordel.wtf/">https://bordel.wtf</a>
+          <li className="mb-2 text-xl">
+            <a target="_blank" rel="noreferrer" href="https://bordel.wtf/">
+              https://bordel.wtf
+            </a>
           </li>
-          <li className="mb-2">
-            <a href="https://797.io/themerge/">https://797.io/themerge</a>
+          <li className="mb-2 text-xl">
+            <a target="_blank" rel="noreferrer" href="https://797.io/themerge/">
+              https://797.io/themerge
+            </a>
           </li>
         </ul>
 
-        <p></p>
-
-        <p class="text-4xl mb-8">Watch the merge live</p>
-        <a href="https://ethglobal.tv/the-merge">
-          <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">
-            Official Merge Watchparty
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://ethglobal.tv/the-merge"
+        >
+          <button class="bg-blue-500 hover:bg-blue-700 text-white text-4xl font-bold py-6 px-12 border border-blue-700 rounded-lg mb-24">
+            Merge Watchparty
           </button>
         </a>
 
-        <p class="text-4xl mb-8">Guide to lots of stuff happening 👇</p>
-        <TwitterTweetEmbed tweetId={"1570127028887715840"} />
+        <p class="text-4xl mb-4">Guide to lots of stuff happening 👇</p>
+        <TwitterTweetEmbed
+          tweetId={"1570127028887715840"}
+          options={{ width: "1000px" }}
+        />
+        <img src={mergeImage} class="max-w-screen m-8 mt-24" alt="merge" />
       </header>
     </div>
   );
